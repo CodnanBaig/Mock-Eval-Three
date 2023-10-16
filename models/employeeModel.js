e9ac983cd9f1af35553de283c1ac3c19c94dc618
Adnan Baig <88790490+CodnanBaig@users.noreply.github.com>
2023-10-16 23:11:16 +0530

    Update employeeModel.js

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -5,7 +5,7 @@ const Employee = new mongoose.Schema({
     lastName: {type: String, required: true},
     email: {type: String, required: true},
     department: {type: String, required: true},
-    salary: {type: Number, required: true},
+    salary: {type: String, required: true},
 })
 
 module.exports = mongoose.model("Employee", Employee);
